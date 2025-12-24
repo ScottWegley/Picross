7bc48d5d793287ce158b03fb9675aa49dad313ea
Scott Wegley <scbrwegley@protonmail.ch>
2025-12-23 22:44:19 -0700

    main class + some debugging of board creation and toString

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import './styles/tailwind.css'
 import App from './App.svelte'
+import { Board } from './lib/Board.ts'
 
 const app = new App({
   target: document.getElementById('app') as HTMLElement
 })
+
+const main: (() => void) = () => {
+  let PicrossBoard = new Board(5, 5);
+  console.log(PicrossBoard.toString());
+  PicrossBoard.fillCell(0, 3, true);
+  PicrossBoard.fillCell(0, 1, true);
+  PicrossBoard.fillCell(0,2,true);
+  PicrossBoard.fillCell(1, 0, true);
+  PicrossBoard.fillCell(1, 4, true);
+  PicrossBoard.fillCell(2, 0, true);
+  PicrossBoard.calculateHints();
+  console.log(PicrossBoard.toString(true));
+}
+
+main();
 
 export default app
