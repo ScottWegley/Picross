6e405fa40a9ad9f0deb6796389f9a420bf3233e4
Scott Wegley <scbrwegley@protonmail.ch>
2025-12-24 23:05:50 -0700

    hovered cell tracking w visual indicator

--- src/components/BoardDisplay.svelte
+++ src/components/BoardDisplay.svelte
@@ -10,13 +10,23 @@
   $: cellSize = maxDim <= 10 ? 32 : 
                 maxDim <= 20 ? 24 : 
                 maxDim <= 30 ? 20 : 16; // pixels
+  
+  let hoveredCell: number | null = null;
 </script>
 
 <div class="p-2">
   <div class="bg-base-200 p-2 overflow-auto flex justify-center">
     <div class="grid gap-0" style="grid-template-columns: repeat({PicrossBoard.cols}, {cellSize}px);">
       {#each cells as _, i}
-        <div class="border bg-white" style="width: {cellSize}px; height: {cellSize}px;"></div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div 
+          class="border" 
+          class:bg-white={hoveredCell !== i}
+          class:bg-orange-400={hoveredCell === i}
+          style="width: {cellSize}px; height: {cellSize}px;"
+          on:mouseenter={() => hoveredCell = i}
+          on:mouseleave={() => hoveredCell = null}
+        ></div>
       {/each}
     </div>
   </div>
